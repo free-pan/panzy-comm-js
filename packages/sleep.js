a32960f6366784cd7c07847f73a43426ce484ea3
pan-zy <sd4015700@yeah.net>
2021-07-20 07:41:47 +0800

    docs: 更新在线文档

--- packages/sleep.js
+++ packages/sleep.js
@@ -5,7 +5,7 @@
 /**
  * 用于在async方法中, 实现类似休眠效果
  * @function
- * @param {number} sleepTime 休眠时间. 单位:毫秒 默认:300毫秒
+ * @param {number} [sleepTime=300] sleepTime 休眠时间. 单位:毫秒 默认:300毫秒
  * @return {Promise<unknown>}
  */
 export const sleep = (sleepTime = 300) => {
